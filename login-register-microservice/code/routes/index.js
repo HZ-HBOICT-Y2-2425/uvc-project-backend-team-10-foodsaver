b0e24edbd68ac8bb88916f9423fcc7a7de4e5b0d
Serhii Popov <popo0016@hz.nl>
2025-01-04 21:52:09 +0000

    user can change name or password in the profile

--- login-register-microservice/code/routes/index.js
+++ login-register-microservice/code/routes/index.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const { registerUser, loginUser, authenticateToken } = require('../controllers/userController');
+const { registerUser, loginUser, authenticateToken, updateUsername, changePassword } = require('../controllers/userController');
 const router = express.Router();
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
+router.post('/update-username', authenticateToken, updateUsername);
+router.post('/change-password', authenticateToken, changePassword);
 
 module.exports = router;
-
-
