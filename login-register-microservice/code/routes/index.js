42157119e03aec65c1456dc622727412b3a923b6
AlbertoooMP <159181260+AlbertoooMP@users.noreply.github.com>
2025-01-09 16:31:27 +0100

    leaderboard added to develop

--- login-register-microservice/code/routes/index.js
+++ login-register-microservice/code/routes/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { registerUser, loginUser, authenticateToken, updateUsername, changePassword, incrementRecipeCount, updateUserSavings } = require('../controllers/userController');
+const { registerUser, loginUser, authenticateToken, updateUsername, changePassword, incrementRecipeCount, updateUserSavings, getTop50UsersCO2Reduced, getTop50UsersMoneySaved } = require('../controllers/userController');
 
 const router = express.Router();
 
@@ -8,6 +8,8 @@ router.post('/login', loginUser);
 router.post('/update-username', authenticateToken, updateUsername);
 router.post('/change-password', authenticateToken, changePassword);
 router.post('/increment-recipe-count', authenticateToken, incrementRecipeCount);
+router.get('/money-leaderboard', getTop50UsersMoneySaved);
+router.get('/co2-leaderboard', getTop50UsersCO2Reduced);
 
 router.put('/users/:id/savings', updateUserSavings);
 
